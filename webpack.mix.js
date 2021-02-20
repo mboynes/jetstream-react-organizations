256cd70cef5523f0f043f80000c296dc196f83aa
Matthew Boynes <git@mattboynes.com>
2021-02-19 19:10:20 -0500

    Build stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,8 @@ mix.js('resources/js/app.js', 'public/js')
         require('tailwindcss'),
         require('autoprefixer'),
     ])
-    .webpackConfig(require('./webpack.config'));
+    .webpackConfig(require('./webpack.config'))
+    .sourceMaps();
 
 if (mix.inProduction()) {
     mix.version();
